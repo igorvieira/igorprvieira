9775ef7496242eb037e53df3e06d80493233b7a0
Igor Vieira <igor.p.r.vieira@gmail.com>
2024-11-23 13:38:25 -0300

    test rss

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Igor Vieira";
-export const SITE_DESCRIPTION = "Notes, Software Engineer, and Web Developer";
+export const SITE_DESCRIPTION =
+  "Notes on technology, web development, and the craft of software engineering";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,8 +6,10 @@ export async function get(context) {
   const posts = await getCollection("blog");
   return rss({
     title: "🍋 Igor Vieira",
-    description: `${SITE_DESCRIPTION} talking about web development, tech, and more.`,
+    description: `${SITE_DESCRIPTION}`,
     site: context.site,
+    image:
+      "https://pbs.twimg.com/profile_images/1849870538954637314/1TNGSEAI_400x400.png",
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
